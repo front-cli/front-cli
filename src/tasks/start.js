e653c326e8d7a68d2a585485306360e4ffb42323
Victor Bastos <victorwpbastos@gmail.com>
2016-10-08 17:50:45 -0300

    fix args with spaces in start task

--- src/tasks/start.js
+++ src/tasks/start.js
@@ -1,13 +1,13 @@
 let path = require('path');
 let toArgv = require('object-to-argv');
-let exec = require('child_process').spawn;
+let spawn = require('child_process').spawn;
 
 module.exports = function(argv) {
 	let command = path.resolve(__dirname, '../../node_modules/.bin/webpack-dev-server');
 	let config = path.resolve(process.cwd(), 'build/webpack.config.dev.js');
 
 	argv.port = argv.port || 3000;
-	argv.config = argv.config || config;
+	argv.config = argv.config || `"${config}"`;
 
 	if (argv.hot === undefined) {
 		argv.hot = true;
@@ -25,5 +25,5 @@ module.exports = function(argv) {
 		argv.progress = true;
 	}
 
-	let proc = exec(command, toArgv(argv), { stdio: 'inherit', shell: true });
+	spawn(`"${command}"`, toArgv(argv), { stdio: 'inherit', shell: true });
 };
