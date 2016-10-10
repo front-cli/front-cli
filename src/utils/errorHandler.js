ff804324b6d9ff702d48f9f5d8de857bc8d7f4ef
Victor Wilson Pinto Bastos <vbastos@sorocaba.sp.gov.br>
2016-10-10 19:37:42 -0300

    better error messages

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,14 +1,21 @@
 let chalk = require('chalk');
 
-module.exports = function (message) {
-	return function(details) {
-		console.log(chalk.red(message));
+let errorMessages = {
+	init: 'An error occurred while creating the application!',
+	start: 'An error occurred while starting the application! Are you in a wrong folder, maybe?',
+	build: 'An error occurred while building the application! Are you in a wrong folder, maybe?',
+	deploy: 'An error occurred while deploying the application! Are you in a wrong folder, maybe?'
+};
 
-		if (details) {
-			console.log();
-			console.log(chalk.gray('Error details:'));
-			console.log();
-			console.log(details);
-		}
-	};
+module.exports = function (type, details) {
+	let message = errorMessages[type];
+
+	console.log(chalk.red(message));
+
+	if (details) {
+		console.log();
+		console.log(chalk.gray('Error details:'));
+		console.log();
+		console.log(details);
+	}
 };
