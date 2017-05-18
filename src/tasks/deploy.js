8fa72e51271200a5bd5243e4e9ea3d8c44b7dccd
Victor Bastos <vbastos@sorocaba.sp.gov.br>
2017-05-18 19:12:04 -0300

    task deploy.

--- src/tasks/deploy.js
+++ src/tasks/deploy.js
@@ -1,5 +1,13 @@
-let chalk = require('chalk');
+let path = require('path');
+let errorHandler = require('../utils/errorHandler');
 
-module.exports = function() {
-	console.log(chalk.red.bold('Task not implemented yet'));
+module.exports = function(argv) {
+	try {
+		let deployScriptPath = path.resolve(process.cwd(), `build/deploy.js`);
+
+		require(deployScriptPath)(require, argv);
+
+	} catch (error) {
+		errorHandler('deploy', error);
+	}
 };
