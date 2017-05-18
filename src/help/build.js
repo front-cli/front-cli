c43a28673b8f090741b579f5c521f51aff11fc6b
Victor Bastos <vbastos@sorocaba.sp.gov.br>
2017-05-18 18:55:43 -0300

    show help for build and deploy tasks.

---
+++ src/help/build.js
@@ -0,0 +1,11 @@
+module.exports = function() {
+	console.log('  Usage:\n');
+	console.log('      cd <appName>');
+	console.log('      front build');
+
+	console.log();
+
+	console.log('  Options:\n');
+	console.log('      --no-notify    disable compilation notifications');
+	console.log('      --verbose      show compilation details');
+};
---
+++ src/help/deploy.js
@@ -0,0 +1,10 @@
+module.exports = function() {
+	console.log('  Usage:\n');
+	console.log('      cd <appName>');
+	console.log('      front deploy');
+
+	console.log();
+
+	console.log('  Options:\n');
+	console.log('      all options would be passed to build/deploy.js');
+};
