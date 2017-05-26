35a6417dd70d9f1ac27fc3df3f81ffc6702ce6cd
Victor Bastos <vbastos@sorocaba.sp.gov.br>
2017-05-26 16:51:24 -0300

    list template repositories ordered by creation date.

--- src/utils/listTemplatesRepositories.js
+++ src/utils/listTemplatesRepositories.js
@@ -3,8 +3,9 @@ let request = require('request');
 module.exports = function (callback) {
 	let url = 'https://api.github.com/users/front-templates/repos';
 	let headers = { 'User-Agent': 'front-cli' };
+	let qs = { 'order': 'created', 'direction': 'desc' };
 
-	request({ url, headers }, (error, response, data) => {
+	request({ url, headers, qs }, (error, response, data) => {
 		if (error) { return callback(error); }
 
 		callback(null, JSON.parse(data));
